8a63c9c350e391bb7b056016a0d942d1dcfb9231
૮꒰ྀི つ˘ `⸝⸝ ꒱ྀིა   ♡ <110964402+noelbluu@users.noreply.github.com>
2023-08-09 10:44:41 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Panide Suthapakti"
+            studentId="650610790"
           />
         </div>
       </body>
