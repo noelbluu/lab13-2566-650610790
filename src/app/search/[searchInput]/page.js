c98df06a800b492b608df74d18c5869741d99a30
૮꒰ྀི つ˘ `⸝⸝ ꒱ྀིა   ♡ <110964402+noelbluu@users.noreply.github.com>
2023-08-09 10:48:39 +0700

    Update page.js

--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -17,14 +17,31 @@ export default function SearchResultPage() {
     you code here...
   );
   */
+  const searchInput = params.searchInput
+  const processedSearchInput = searchInput.replaceAll("%20", " ")
+  const filteredMovies = movieDB.filter((movie) =>
+    movie.title.toLowerCase().includes(processedSearchInput.toLowerCase())
+  );
 
   return (
     <div>
       <p className="fw-bold fs-4 text-center my-0">
-        Searching &quot; ... &quot;
+        Searching &quot; {processedSearchInput} &quot;
       </p>
-      <p className="fw-bold fs-4 text-center">Found ... result(s)</p>
+      <p className="fw-bold fs-4 text-center">
+        Found {filteredMovies.length} result(s)
+      </p>
       {/* Use  "filteredMovies" variable to map-loop rendering MovieRow component */}
+      {filteredMovies.map((movie, i) => (
+        <MovieRow
+          key={movie.id}
+          id={movie.id}
+          title={movie.title}
+          detail={movie.detail}
+          rating={movie.rating}
+          number={i + 1}
+        />
+      ))}
     </div>
   );
 }
